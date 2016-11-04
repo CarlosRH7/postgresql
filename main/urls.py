ee8f6af16560dc11891a252128d61b686799c36e
Carlos <carlos@la-MacBook-Air-de-Carlos.local>
2016-11-04 10:20:13 -0600

    algo

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+	url(r'^$',views.Home.as_view(),name="home"),
+	# url pra  detalles
+	# url(r'^(?p<id>[0-9]+)/$', views.DetailView.as_view(), name="detalle")
+]
