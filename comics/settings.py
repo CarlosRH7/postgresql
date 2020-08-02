0adec3afebfbe756e86b6fee4c0af0aa4b111862
Carlos Ramirez <c.ramirez@aqtiva.mx>
2020-08-02 14:46:31 -0500

    Add requirements.txt

--- comics/settings.py
+++ comics/settings.py
@@ -76,24 +76,23 @@ WSGI_APPLICATION = 'comics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# DATABASES={
+#     'default':{
+#         'ENGINE':'django.db.backends.postgresql_psycopg2',
+#         'NAME':'',
+#         'USER':'',
+#         'PASSWORD':'',
+#         'HOTS':'localhost',
+#         'PORT':'',
 #     }
 # }
-
-DATABASES={
-    'default':{
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'baseUno',
-        'USER':'carlos',
-        'PASSWORD':'12345',
-        'HOTS':'localhost',
-        'PORT':'',
-    }
-
-}
 
 
 # Password validation
